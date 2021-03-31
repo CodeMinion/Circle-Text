c0bab89d662cf685901542427f2db06b5299db38
Frank E. Hernandez <hernandez.frank@gmail.com>
2021-03-30 22:53:26 -0400

    Added sample print code.

--- app/src/main/java/com/codeminion/circletext/MainActivity.kt
+++ app/src/main/java/com/codeminion/circletext/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.codeminion.circletext
+
+import android.bluetooth.BluetoothAdapter
 import android.graphics.*
 import android.os.Bundle
 import android.provider.MediaStore
@@ -6,22 +8,37 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-
+import com.brother.ptouch.sdk.LabelInfo
+import com.brother.ptouch.sdk.Printer
+import com.brother.ptouch.sdk.PrinterInfo
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var mGeneratedBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val generateButton: View = findViewById(R.id.generate_text)
-        val topText:EditText = findViewById(R.id.top_text)
-        val bottomText:EditText = findViewById(R.id.bottom_text)
+        val topText: EditText = findViewById(R.id.top_text)
+        val bottomText: EditText = findViewById(R.id.bottom_text)
+        val printButton: View = findViewById(R.id.print_image)
 
         generateButton.setOnClickListener {
-            generateBitmap(topText = topText.text.toString(), bottomText = bottomText.text.toString())
+            generateBitmap(
+                topText = topText.text.toString(),
+                bottomText = bottomText.text.toString()
+            )
         }
+
+        printButton.setOnClickListener {
+            mGeneratedBitmap?.let { image ->
+                print(image)
+            }
+        }
+
         // IMPORTANT - This is for sample purposes only.
         // The work in this sample if being done on the main thread to not
         // distract the readers from the main subject.
@@ -31,7 +48,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun generateBitmap(topText:String = "Brother Hackathon", bottomText:String = "2021") {
+    fun generateBitmap(topText: String = "Brother Hackathon", bottomText: String = "2021") {
+
+        mGeneratedBitmap?.let {
+            if (!it.isRecycled) {
+                it.recycle()
+            }
+        }
+
         val imageBitmap = BitmapFactory.decodeResource(resources, R.drawable.logo_brother)
 
         val imageView: ImageView = findViewById(R.id.image)
@@ -65,7 +89,6 @@ class MainActivity : AppCompatActivity() {
         val imageBounds = RectF(0f, 0f, totalWidth.toFloat(), totalWidth.toFloat())
 
 
-
         // Background - Draw circle Bitmap
         canvas.drawARGB(0, 0, 0, 0);
         canvas.drawCircle(totalWidth / 2f, totalWidth / 2f, totalWidth / 2f, imagePaint)
@@ -74,7 +97,8 @@ class MainActivity : AppCompatActivity() {
         val srcRect = Rect(0, 0, imageSize, imageSize)
         val backgroundSize = (totalWidth * 0.8).toInt()
         val padding = (totalWidth - backgroundSize) / 2
-        val destRect = Rect(padding, padding, padding + backgroundSize, padding + backgroundSize)
+        val destRect =
+            Rect(padding, padding, padding + backgroundSize, padding + backgroundSize)
         canvas.drawBitmap(imageBitmap, srcRect, destRect, imagePaint)
 
         // Background Draw a Rectangle for printer support.
@@ -100,8 +124,41 @@ class MainActivity : AppCompatActivity() {
 
         imageView.setImageBitmap(circleImage)
 
+        mGeneratedBitmap = circleImage
+
         // Save to gallery
-        MediaStore.Images.Media.insertImage(getContentResolver(), circleImage, "Circle-Text" , "Sample Circle Text");
+        MediaStore.Images.Media.insertImage(
+            getContentResolver(),
+            circleImage,
+            "Circle-Text",
+            "Sample Circle Text"
+        )
+
+
+    }
+
+    // IMPORTANT: This is done on the main thread for demo purposes only.
+    // Move this to a background thread.
+    fun print(bitmap: Bitmap) {
+        val printer = Printer()
+        val printInfo = PrinterInfo()
+        printInfo.printerModel = PrinterInfo.Model.QL_1110NWB
+        printInfo.printMode = PrinterInfo.PrintMode.FIT_TO_PAGE
+        printInfo.isAutoCut = true
+        printInfo.port = PrinterInfo.Port.BLUETOOTH;
+        printInfo.macAddress = "58:93:D8:BD:69:95" // Printer BLuetooth Mac
+        printInfo.workPath = filesDir.absolutePath + "/";
+        printInfo.labelNameIndex = LabelInfo.QL1100.W103.ordinal;
+
+        printer.printerInfo = printInfo
+        printer.setBluetooth(BluetoothAdapter.getDefaultAdapter())
+
+        printer.startCommunication()
+        val result = printer.printImage(bitmap)
+        if (result.errorCode != PrinterInfo.ErrorCode.ERROR_NONE) {
+            println("Error: " + result.errorCode)
+        }
+        printer.endCommunication()
 
     }
 }
